535f3b81c43bc0bd04ca7605fbf672646b5dedb0
moemi ohtsu <s0623192@gmail.com>
2019-06-26 14:31:13 +0900

    Add: rails newで新規アプリケーション作成、config/application.rbでscaffoldの際に余計なファイルが生成されないよう記述

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TaskTraining
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+    # タイムゾーンを日本語設定
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    # scaffoldで余計なファイルが生成されないよう記述
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
+  end
+end
