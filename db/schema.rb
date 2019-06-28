2f9cf563c89fb0044124b53378205505a6bbd7aa
moemi ohtsu <s0623192@gmail.com>
2019-06-28 19:02:18 +0900

    Add: カラム名変更マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_083757) do
+ActiveRecord::Schema.define(version: 2019_06_28_095403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "task_title"
-    t.text "task_content"
+    t.string "title"
+    t.text "content"
   end
 
 end
