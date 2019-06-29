022eab95b29e447eba745e79c22ab5482d410fcd
moemi ohtsu <s0623192@gmail.com>
2019-06-29 22:03:18 +0900

    Add: 編集機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
 
   def index
     @tasks = Task.all
@@ -19,13 +20,27 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを更新しました"
+    else
+      render 'edit'
+    end
   end
 
   private
   
   def task_params
     params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
