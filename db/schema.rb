8627ed4c29bc59151b91670c103370b0beec98c4
moemi ohtsu <s0623192@gmail.com>
2019-07-04 14:20:44 +0900

    Add: tasksテーブルにcreated_at,updated_atカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_095403) do
+ActiveRecord::Schema.define(version: 2019_07_04_045855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_06_28_095403) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
