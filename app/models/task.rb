effbfde731f918e07dcc958a6b1a68259927560e
moemi ohtsu <s0623192@gmail.com>
2019-07-08 21:47:06 +0900

    Fix: contentのuniqueness: true を削除（空欄でアップしようとした時に、他のタスクが空欄だとこのバリデーションが発動してしまうため。

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,4 @@ class Task < ApplicationRecord
   # 内容が空／同じものの重複
   validates :title, presence: true, uniqueness: true
   validates :title, length: { maximum: 101 }
-  validates :content, uniqueness: true 
 end
