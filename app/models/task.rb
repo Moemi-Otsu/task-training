a004cf78602ddbfd1a67dd3aaebff718849c4d3e
moemi ohtsu <s0623192@gmail.com>
2019-07-08 20:49:27 +0900

    Add: バリデーションのmodelSpecを記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   # 内容が空／同じものの重複
   validates :title, presence: true, uniqueness: true
+  validates :title, length: { maximum: 101 }
+  validates :content, uniqueness: true 
 end
