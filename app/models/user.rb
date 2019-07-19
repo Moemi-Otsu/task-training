c989ad08dac865a75c3a5222c64dee7281e4b53d
moemi ohtsu <s0623192@gmail.com>
2019-07-19 19:45:48 +0900

    Add: modelにバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates: name, presense: true, length: { maximum: 30 }
+  validates: email, presense: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
