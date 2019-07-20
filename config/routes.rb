720e7edddf1770a79416f3d0a6d49b13041940f7
moemi ohtsu <s0623192@gmail.com>
2019-07-20 16:35:38 +0900

    Add: ユーザーログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
   # root 'tasks#index'
   resources :tasks do
     collection do
       post :confirm
     end
   end
-  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  # resources :users, only: [:new, :create, :show]
 end
