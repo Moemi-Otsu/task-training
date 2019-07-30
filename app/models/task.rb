66cc060fa10ba2884847a2d98b8d6b1cc0f2265e
moemi ohtsu <s0623192@gmail.com>
2019-07-30 17:37:16 +0900

    Add: 中間テーブルlabelやuser,taskモデルに多対多アソシエーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :labels, dependent: :destroy
 
   # 内容が空／同じものの重複
   validates :title, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_071636) do
+ActiveRecord::Schema.define(version: 2019_07_30_081528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "task_id"
+    t.string "label_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
