f76796ae6bb9bf6197766f1eb7542e62776b5383
moemi ohtsu <s0623192@gmail.com>
2019-07-30 14:45:51 +0900

    Fix: 管理者ユーザーが残り1名になった時に編集・削除できないようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :admin_last_user_delete_judge, only: [:update]
 
   def new
     if session[:user_id].present?
@@ -13,7 +14,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user.id)
+      # redirect_to user_path(@user.id)
+      redirect_to tasks_path
     else
       render 'new'
     end
@@ -27,22 +29,23 @@ class UsersController < ApplicationController
     user_not_logged_in
   end
 
+  # adminの機能
   def update
-    if @user.update(user_params)
-      redirect_to admin_users_path, notice: 'ユーザー情報を編集しました！'
-    else
-      render 'admin/users/edit'
-    end
   end
 
+  # adminの機能
   def destroy
     user_admin = User.where(admin: true)
-    # require 'byebug'; byebug
-    if user_admin.size >= 1 && @user.admin.blank?
+    if @user.admin.blank?
       @user.destroy
       redirect_to admin_users_path, notice: 'ユーザーを削除しました'
     else
-      redirect_to admin_users_path, notice: '管理者ユーザー不在を防止するため、削除できませんでした。'
+      if user_admin.size > 1
+        @user.destroy
+        redirect_to admin_users_path, notice: 'ユーザーを削除しました'
+      else
+        redirect_to admin_users_path, notice: '管理者ユーザー不在を防止するため、削除できませんでした。'
+      end
     end
   end
 
@@ -59,6 +62,27 @@ class UsersController < ApplicationController
   def user_not_logged_in
     unless current_user.id == @user.id
       redirect_to new_session_path
+    end
+  end
+
+  def update_user
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: 'ユーザー情報を編集しました！'
+    else
+      render 'admin/users/edit'
+    end
+  end
+
+  def admin_last_user_delete_judge
+    if user_params[:admin] == "false"
+      if User.where(admin: true).size > 1
+        update_user
+      else
+        flash.now[:alert] = "管理者ユーザー不在を防止するため、編集できませんでした。"
+        render 'admin/users/edit'
+      end
+    else
+      update_user
     end
   end
 
